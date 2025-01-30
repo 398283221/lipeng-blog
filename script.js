c5ff6d7bfc88dd400bfba1cc79ea45846089b454
398283221 <cheng398283221@qq.com>
2025-01-30 21:30:30 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ const toggleButton = document.getElementById('night-mode-toggle');
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('night-mode');
-    document.querySelectorAll('a').forEach(link => link.classList.toggle('night-mode'));
-    document.querySelector('header').classList.toggle('night-mode');
-    document.querySelector('footer').classList.toggle('night-mode');
-    document.querySelector('button').classList.toggle('night-mode');
 });
